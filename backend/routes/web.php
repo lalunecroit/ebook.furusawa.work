105df06a650f1253e46920fcbd924fb959537300
Yuuki Furusawa <lalunecroit182@gmail.com>
2026-09-24 08:15:31 +0900

    [Step09] backend: admin ホストは管理画面にリダイレクト

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// adminホストは、管理画面パス(/admin/books)へリダイレクト
+if ($adminHost = config('app.admin_host')) {
+    Route::domain($adminHost)->group(function (): void {
+        Route::redirect('/', '/admin/books');
+    });
+}
+
 Route::get('/', function () {
     return view('welcome');
 });
